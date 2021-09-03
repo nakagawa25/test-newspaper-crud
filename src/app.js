aac1d37f2f59fc932a4d41430f58789b36c46903
Lucas Nakagawa <lucasnakagawanascimento@gmail.com>
2021-09-03 15:28:09 -0300

    fix: Add Cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const routes = require('./routes');
 const app = express();
+const cors = require('cors');
+
+
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+
+    res.header("Access-Control-Allow-Headers", "origin, x-requested-with, content-type");
+
+    app.use(cors());
+    next();
+});
 
 app.use(express.json());
 app.use(routes);
